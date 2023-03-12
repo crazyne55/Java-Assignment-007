2197c36ac7cd3678e9ac0f61a49a90449ed9a126
crazyne55 <95332356+crazyne55@users.noreply.github.com>
2023-03-12 12:10:49 -0700

    the flow chart made it so much easier to understand

--- src/MyMath.java
+++ src/MyMath.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
 
+/**
+ * @author crazyne55
+ *
+ * 3/12/2023 @ 1210
+ */
 public class MyMath {
-
+    static Scanner s = new Scanner(System.in);
+    public static int gcf(int a, int b) {
+        int c;
+        while (b!=0) {
+            if(a>b) {
+                c = b;
+                b = a % b;
+                a = c;
+            }else {
+                b = b % a;
+            }
+        }
+        return a;
+    }
     public static void main(String[] args) {
+        System.out.println("Enter 2 integer values to calculate the GCF");
+        int a, b, c;
+        System.out.print("1: ");
+        a = Integer.parseInt(s.nextLine());
+        System.out.print("2: ");
+        b = Integer.parseInt(s.nextLine());
+        c = gcf(a,b);
+        System.out.printf("The GCF of %s and %s is %s%n",a,b,c);
     }
 }
